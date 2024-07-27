21603f292907412f9373eab61d4744c5670f022a
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-07-26 18:57:37 -0700

    Equality printer Challenge

--- src/Challenges/MinuteToYearAndDay.java
+++ src/Challenges/MinuteToYearAndDay.java
@@ -3,14 +3,16 @@ package Challenges;
 public class MinuteToYearAndDay {
     public static void main(String[] args) {
         printYearsAndDays(42545353);
-    } public static void printYearsAndDays(long minutes){
-        if(minutes < 0){
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
             System.out.print("Invalid Value");
             return;
         }
 
         long day = (minutes / (24 * 60)) % 365;
-        long year = minutes / (60 * 24 *365);
-        System.out.print(minutes +" min = "+ year + " y and "+day +" d");
+        long year = minutes / (60 * 24 * 365);
+        System.out.print(minutes + " min = " + year + " y and " + day + " d");
     }
 }
