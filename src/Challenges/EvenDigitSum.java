e92e5ea1f350c2090316e4af27bb812657316920
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-03 01:07:55 -0700

    Even Digit Sum

---
+++ src/Challenges/EvenDigitSum.java
@@ -0,0 +1,25 @@
+package Challenges;
+
+public class EvenDigitSum {
+    public static void main(String[] args) {
+     System.out.println(getEvenDigitSum(122));
+    }
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        for(int i = number; i > 0; i/=10){
+            int lastDigit = i % 10;
+
+            if(lastDigit % 2 == 0){
+
+                sum += lastDigit;
+            }
+        }
+
+        return sum;
+    }
+}
