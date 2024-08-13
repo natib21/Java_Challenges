4cc8588bfef587468345950601324337a35ffa22
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-12 23:36:46 -0700

    Challenge on getter and setter

---
+++ src/Challenges/PersonC.java
@@ -0,0 +1,14 @@
+package Challenges;
+
+public class PersonC {
+
+    public static void main(String[] args) {
+        Person per = new Person();
+        per.setFirstName("Nathnael");
+
+        System.out.println("teen = " + per.isTeen());
+        System.out.println("fullName= " + per.getFullName());
+    }
+
+
+}
