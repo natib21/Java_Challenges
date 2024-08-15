47de4e9cf5ee66269000892e0314085a6d6ad48f
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-15 02:13:37 -0700

    Point Challenge

---
+++ src/Challenges/PointMain.java
@@ -0,0 +1,8 @@
+package Challenges;
+
+public class PointMain {
+    public static void main(String[] args) {
+        Point pt = new Point(6,5);
+        System.out.println("distance(0,0)= " + pt.distance());
+    }
+}
