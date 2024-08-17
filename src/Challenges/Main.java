a67a2314b9ba19ce07c0e11f211a75c6fe3b74e9
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-17 04:32:31 -0700

    Java Inheritance Challenge

---
+++ src/Challenges/Main.java
@@ -0,0 +1,9 @@
+package Challenges;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+    }
+}
